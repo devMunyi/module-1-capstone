6965637a5965a4a1212069ea30e478150e7140e0
devMunyi <samunyi90@gmail.com>
2022-09-16 12:53:34 +0300

    add requested changes

--- js/main.js
+++ js/main.js
@@ -21,54 +21,48 @@ const rooms = [
     id: 1,
     imageUrl: './img/room-2.jpg',
     name: 'MD Capital 15959',
-    maxCount: 3,
-    facilities: ['Parking', 'Free WiFI', 'Entertainment'],
     type: 'Delux',
+    desc: 'Night Curfew in Nairobi | Check-in allowed between 05:00 AM to 09:00 PM Couples are welcome Guests can check in using any local or outstation ID proof.',
   },
 
   {
     id: 2,
     imageUrl: './img/room-1.jpg',
     name: 'MD Flagship 75243',
-    maxCount: 2,
-    facilities: ['Parking', 'Free WiFI', 'Entertainment'],
     type: 'Non-Delux',
+    desc: 'Night Curfew in Nairobi | Check-in allowed between 05:00 AM to 09:00 PM Couples are welcome Guests can check in using any local or outstation ID proof.',
   },
 
   {
     id: 3,
     imageUrl: './img/room-3.jpg',
     name: 'MD FLAGSHIP 76687',
-    maxCount: 3,
-    facilities: ['Parking', 'Free WiFI', 'Entertainment'],
     type: 'Non-Delux',
+    desc: 'Night Curfew in Nairobi | Check-in allowed between 05:00 AM to 09:00 PM Couples are welcome Guests can check in using any local or outstation ID proof.',
   },
 
   {
     id: 4,
     imageUrl: './img/room-4.jpg',
     name: 'MD SPOT 37669',
-    maxCount: 2,
-    facilities: ['Parking', 'Free WiFI'],
     type: 'Delux',
+    desc: 'Night Curfew in Nairobi | Check-in allowed between 05:00 AM to 09:00 PM Couples are welcome Guests can check in using any local or outstation ID proof.',
   },
 
   {
     id: 5,
     imageUrl: './img/room-5.jpg',
     name: 'MD 80211 Collection',
-    maxCount: 2,
-    facilities: ['Parking', 'Free WiFI'],
     type: 'Non-Delux',
+    desc: 'Night Curfew in Nairobi | Check-in allowed between 05:00 AM to 09:00 PM Couples are welcome Guests can check in using any local or outstation ID proof.',
   },
 
   {
     id: 6,
     imageUrl: './img/room-6.jpg',
     name: 'MD Flagship 82898',
-    maxCount: 3,
-    facilities: ['Parking', 'Free WiFI'],
     type: 'Delux',
+    desc: 'Night Curfew in Nairobi | Check-in allowed between 05:00 AM to 09:00 PM Couples are welcome Guests can check in using any local or outstation ID proof.',
   },
 ];
 
@@ -77,22 +71,28 @@ const rooms = [
 
 const loadRooms = () => {
   // get the element where to add dynamic html
-  const roomsContainer = document.getElementById('rooms-container');
+  const roomsContainer = document.querySelector('.wrap-rooms');
   let row = '';
   for (let i = 0; i < rooms.length; i += 1) {
-    row += `
-    <div class="row mb-3">
-    <div class="col-md-6">
-      <img class="img-fluid" src="${rooms[i].imageUrl}" alt="Room Two" />
-    </div>
-  
-    <div class="col-md-6">
-      <h3>${rooms[i].name}</h3>
-      <p>Facilities: <b>${rooms[i].facilities.join(', ')}</b></p>
-      <p>Max Count: ${rooms[i].maxCount}</p>
-      <p>Type: ${rooms[i].type}</p>
-    </div>
-  </div>`;
+    row += `<div class="room">
+      <div class="room-img">
+        <img
+          class=""
+          src="${rooms[i].imageUrl}"
+          alt="Room One"
+          width="178"
+          height="177"
+        />
+      </div>
+      <div class="room-details">
+        <h5 class="room-name">${rooms[i].name}</h5>
+        <p class="room-type">${rooms[i].type}</p>
+        <div class="guide-line"></div>
+        <p class="room-desc">
+          ${rooms[i].desc}
+        </p>
+      </div>
+    </div>`;
   }
 
   // insert the content in the dom after for loop finishes
